e9b7af38f4299c557244bcd423239e1893ba989c
francois <francois.nadeau.1@umontreal.ca>
2021-04-13 14:06:17 -0400

    fixed 'multiple_replace.py'

--- multiple_replace.py
+++ multiple_replace.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 
-import re
+import regex as re
 from typing import Union
 
 def multiple_replace(adict:dict, text:Union[str, bytes], encoding:str=None):
+    """ Adapted from source:
+        https://code.activestate.com/recipes/81330-single-pass-multiple-replace/
+    """
   # Create a regular expression  from the dictionary keys
-    encoding = [encoding if encoding else 'utf8'][0]
-    regex = re.compile("(%s)".encode(encoding) % \
-                     "|".encode(encoding).join(map(re.escape, adict.keys())))
+    adict = dict((itm[0].encode(), itm[1].encode())
+                 for itm in new.items())
+    encoding = [encoding if encoding else 'UTF-8'][0]
+    regex_pattern = re.compile("(%s)".encode(encoding) % \
+                     "|".encode(encoding).join(map(re.escape,
+                     adict.keys())))
   # For each match, look-up corresponding value in dictionary
-    return regex.sub(lambda mo: adict[mo.string[mo.start():mo.end()]], text) 
+    mo = b"mo"
+    return (adict, regex_pattern,
+            regex_pattern.sub(lambda mo: \
+                              adict[mo.string[mo.start():mo.end()]],
+                              text.encode()).decode())
 
 if __name__ == "__main__": 
-    multiple_replace(adict, text)
-#   text = "Larry Wall is the creator of Perl"
-
-#   dict = {
-#     "Larry Wall" : "Guido van Rossum",
-#     "creator" : "Benevolent Dictator for Life",
-#     "Perl" : "Python",
-#   } 
-
-#   return multiple_replace(dict, text)
+    multiple_replace(adict, text.encode())
